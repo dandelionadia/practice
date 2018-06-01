1139ce92dd6cac2a0af53395c9dfa72ba55b90da
Nadia Ridko <nadia.ridko@gmail.com>
2018-06-01 19:54:48 +0200

    adds start jquery

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,35 @@
 $(document).ready(function () {
+   var validEmail = false;
 
-});
+  $('form').submit(function(event) {
+    // заборонити щоб форма не відправлялась.щоб був час на перевірку заповнення форми
+    event.preventDefault();
+
+    // отримує значення поля
+    var email = $('#exampleInputEmail1').val();
+
+    if (email == '') {
+      // якщо строка порожня то додаємо клас який покаже помилку
+      $('#exampleInputEmail1').removeClass('is-valid').addClass('is-invalid');
+      // додаємо клас помилки 
+      $('#emailBlock').removeClass('valid-feedback').addClass('invalid-feedback');
+      
+      
+      validName = false;
+    } else {
+      $('#exampleInputEmail1').removeClass('is-invalid').addClass('is-valid');  
+      $('#emailBlock').removeClass('invalid-feedback').addClass('valid-feedback');
+      
+      validEmail = true;
+          
+    }
+
+    // if (validEmail = true) {
+    //   console.log('good');
+    //   // відв'язати подію заборони і  дозволити відправлення форми.
+    //   $('form').unbind('submit').submit();
+    // }
+      
+  })
+})
+
