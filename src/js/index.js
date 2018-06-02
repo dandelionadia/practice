58053c5e123f690e9c9d2729480a546898fb1dd1
Nadia Ridko <nadia.ridko@gmail.com>
2018-06-02 14:59:33 +0200

    input address and phone show about error and success message

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
    var validFirstName = false;
    var validLastName = false;
    var validUsername = false;
+   var validAddress = false;
+   var validPhone = false;
 
   $('form').submit(function(event) {
     // заборонити щоб форма не відправлялась.щоб був час на перевірку заповнення форми
@@ -13,25 +15,15 @@ $(document).ready(function () {
     var firstName = $('#formGroupExampleInputFirstName').val();    
     var lastName = $('#formGroupExampleInputLastName').val();
     var username = $('#inlineFormInputGroupUsername').val();
-
-    if (email == '') {
-      // якщо строка порожня то додаємо клас який покаже помилку
-      $('#exampleInputEmail1').removeClass('is-valid').addClass('is-invalid');
-      // додаємо клас помилки тексту помилки
-      $('.change-info__email').removeClass('valid-feedback').addClass('invalid-feedback');
-      // перезаписати текст
-      $('div.change-info__email').html('Should be the format of the email!');
-      validName = false;
-    } else {
-      $('#exampleInputEmail1').removeClass('is-invalid').addClass('is-valid');  
-      $('.change-info__email').removeClass('invalid-feedback').addClass('valid-feedback');
-      $('div.change-info__email').html('Good!');      
-      validEmail = true;       
-    }
+    var address = $('#inputAddress').val();
+    var phone = $('#exampleInputPhone').val();
 
     if (firstName == '') {
+        // якщо строка порожня то додаємо клас який покаже помилку input
       $('#formGroupExampleInputFirstName').removeClass('is-valid').addClass('is-invalid');
+      // додаємо клас який показує колір текстово повідомлення помилки
       $('.change-info__firstName').removeClass('valid-feedback').addClass('invalid-feedback');
+      // перезаписати текст помилки
       $('div.change-info__firstName').html('Must be more than 3 letters!');      
       validFirstName = false;    
     } else {
@@ -56,13 +48,49 @@ $(document).ready(function () {
     if(username == '') {
       $('#inlineFormInputGroupUsername').removeClass('is-valid').addClass('is-invalid');
       $('.change-info__username').removeClass('valid-feedback').addClass('invalid-feedback');
-      $('div.change-info__username').html('Must be more than 3 letters!');
+      $('div.change-info__username').html('Must be more than 3 characters!');
       validUsername = false;
     } else {
       $('#inlineFormInputGroupUsername').removeClass('is-invalid').addClass('is-valid'); 
       $('.change-info__username').removeClass('invalid-feedback').addClass('valid-feedback');
       $('div.change-info__username').html('Good!');
       validUsername = true; 
+    }
+
+    if (email == '') {
+      $('#exampleInputEmail1').removeClass('is-valid').addClass('is-invalid');
+      $('.change-info__email').removeClass('valid-feedback').addClass('invalid-feedback');
+      $('div.change-info__email').html('Should be the format of the email!');
+      validName = false;
+    } else {
+      $('#exampleInputEmail1').removeClass('is-invalid').addClass('is-valid');  
+      $('.change-info__email').removeClass('invalid-feedback').addClass('valid-feedback');
+      $('div.change-info__email').html('Good!');      
+      validEmail = true;       
+    }
+
+    if (address == '') {
+      $('#inputAddress').removeClass('is-valid').addClass('is-invalid');
+      $('.change-info__address').removeClass('valid-feedback').addClass('invalid-feedback');
+      $('div.change-info__address').html('Must be more than 5 characters!');
+      validName = false;
+    } else {
+      $('#inputAddress').removeClass('is-invalid').addClass('is-valid');  
+      $('.change-info__address').removeClass('invalid-feedback').addClass('valid-feedback');
+      $('div.change-info__address').html('Good!');      
+      validEmail = true;       
+    }
+
+    if (phone == '') {
+      $('#exampleInputPhone').removeClass('is-valid').addClass('is-invalid');
+      $('.change-info__phone').removeClass('valid-feedback').addClass('invalid-feedback');
+      $('div.change-info__phone').html('Must be more than 9 numbers!');
+      validPhone = false;
+    } else {
+      $('#exampleInputPhone').removeClass('is-invalid').addClass('is-valid');  
+      $('.change-info__phone').removeClass('invalid-feedback').addClass('valid-feedback');
+      $('div.change-info__phone').html('Good!');      
+      validPhone = true;       
     }
 
     // if (validEmail = true) {
